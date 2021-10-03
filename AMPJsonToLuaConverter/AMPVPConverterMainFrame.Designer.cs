5cbfdb179e2c9debed24a718b5351229e324400f
CreepyJoker <noaxeqtr@gmail.com>
2021-10-03 18:45:14 +0300

    Presentation Commit.

---
+++ AMPJsonToLuaConverter/AMPVPConverterMainFrame.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ArenaMasterIO_Json_Converter
+{
+    partial class MainFrame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadJsonBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loadJsonBtn
+            // 
+            this.loadJsonBtn.Location = new System.Drawing.Point(26, 24);
+            this.loadJsonBtn.Name = "loadJsonBtn";
+            this.loadJsonBtn.Size = new System.Drawing.Size(91, 23);
+            this.loadJsonBtn.TabIndex = 0;
+            this.loadJsonBtn.Text = "Load JSON";
+            this.loadJsonBtn.UseVisualStyleBackColor = true;
+            this.loadJsonBtn.Click += new System.EventHandler(this.loadJsonBtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Location = new System.Drawing.Point(339, 24);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(96, 23);
+            this.convertBtn.TabIndex = 1;
+            this.convertBtn.Text = "Convert";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            this.convertBtn.Click += new System.EventHandler(this.convertBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "StatusLabel";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(339, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Conv Realms";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 104);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.convertBtn);
+            this.Controls.Add(this.loadJsonBtn);
+            this.Name = "MainFrame";
+            this.Text = "ArenaMaster.IO - JSON to LUA data converter";
+            this.Load += new System.EventHandler(this.MainFrame_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadJsonBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button convertBtn;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AMPJsonToLuaConverter/AMPVPConverterMainFrame.cs
@@ -0,0 +1,261 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ArenaMasterIO_Json_Converter
+{
+    public partial class MainFrame : Form
+    {
+        public MainFrame()
+        {
+            InitializeComponent();
+        }
+        private string jsonString = "";
+        private string outputLUA = "";
+        private string regionFromFile = "";
+        private string origFileName = "";
+        private void MainFrame_Load(object sender, EventArgs e)
+        {
+            label1.Text = "To start, load a JSON file first.";
+            convertBtn.Enabled = false;
+        }
+
+        private void loadJsonBtn_Click(object sender, EventArgs e)
+        {
+            AMPVP_ResetUI();
+
+            var dlg = new OpenFileDialog();
+            
+            dlg.DefaultExt = ".json";
+            dlg.Filter = "Json Files (.json)|*.json";
+            var result = dlg.ShowDialog();
+            if (result != DialogResult.OK) return;
+
+
+            using (StreamReader sr = new StreamReader(dlg.FileName))
+            {
+                label1.Text = "Loading file, please wait...";
+
+                regionFromFile = dlg.SafeFileName.Replace(".json", "");
+                origFileName = dlg.FileName.Replace(".json", "");
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    jsonString = line;
+                    //Console.WriteLine(regionFromFile + ": " + jsonString);
+                }
+                convertBtn.Enabled = true;
+                label1.Text = "File loaded succesfully. You can now Convert it.";
+                sr.Close();
+            }
+            
+        }
+
+        private void convertBtn_Click(object sender, EventArgs e)
+        {
+            AMPVP_ConvertJSONTableFunc(jsonString);
+        }
+
+        private void AMPVP_ResetUI()
+        {
+            convertBtn.Enabled = false;
+            label1.Text = "To start, load a JSON file first.";
+            outputLUA = "";
+            jsonString = "";
+            origFileName = "";
+            regionFromFile = "";
+        }
+
+        private void AMPVP_ConvertJSONTableFunc(string json)
+        {
+            
+            try
+            {
+                
+                var objects = JsonConvert.DeserializeObject<dynamic>(json);
+
+                using (StreamWriter sw = new StreamWriter(origFileName + ".lua"))
+                {
+                    
+                    string startLua = "AMPVP_REGIONDATA_" + regionFromFile + " = { ";
+
+                    sw.WriteLine(startLua);
+
+                    foreach (var obj in objects)
+                    {
+                        var userName = obj.name;
+                        var realm = obj.realm;
+                        var twosExp = obj.exp_2v2;
+                        var tresExp = obj.exp_3v3;
+                        var RBGExp = obj.exp_rbg;
+                        var latest_update = obj.updated_at;
+
+                        var acc_2v2exp = obj.acc_exp_2v2;
+                        var acc_3v3exp = obj.acc_exp_3v3;
+                        var acc_rbgexp = obj.acc_exp_rbg;
+
+                        if (twosExp == null)
+                        {
+                            twosExp = 0;
+                        }
+
+                        if (tresExp == null)
+                        {
+                            tresExp = 0;
+                        }
+
+                        if (RBGExp == null)
+                        {
+                            RBGExp = 0;
+                        }
+
+                        if (acc_2v2exp == null && acc_3v3exp == null && acc_rbgexp == null)
+                        {
+                            outputLUA = "[\"" + userName + "-" + realm + "\"]={"
+                            + "[\"e2s\"]=" + twosExp + ","
+                            + "[\"e3s\"]=" + tresExp + ","
+                            + "[\"eRBG\"]=" + RBGExp + ","
+                            + "[\"lu\"]=" + "\"" + latest_update + "\""
+                            + "},"
+                            ;
+                        }
+
+                        if(acc_2v2exp != null && acc_3v3exp == null && acc_rbgexp == null)
+                        {
+                            outputLUA = "[\"" + userName + "-" + realm + "\"]={"
+                            + "[\"e2s\"]=" + twosExp + ","
+                            + "[\"a2s\"]=" + acc_2v2exp + ","
+                            + "[\"e3s\"]=" + tresExp + ","
+                            + "[\"eRBG\"]=" + RBGExp + ","
+                            + "[\"lu\"]=" + "\"" + latest_update + "\""
+                            + "},"
+                            ;
+                        }
+
+                        if (acc_2v2exp == null && acc_3v3exp != null && acc_rbgexp == null)
+                        {
+                            outputLUA = "[\"" + userName + "-" + realm + "\"]={"
+                            + "[\"e2s\"]=" + twosExp + ","
+                            + "[\"a3s\"]=" + acc_3v3exp + ","
+                            + "[\"e3s\"]=" + tresExp + ","
+                            + "[\"eRBG\"]=" + RBGExp + ","
+                            + "[\"lu\"]=" + "\"" + latest_update + "\""
+                            + "},"
+                            ;
+                        }
+
+                        if (acc_2v2exp == null && acc_3v3exp == null && acc_rbgexp != null)
+                        {
+                            outputLUA = "[\"" + userName + "-" + realm + "\"]={"
+                            + "[\"e2s\"]=" + twosExp + ","
+                            + "[\"e3s\"]=" + tresExp + ","
+                            + "[\"eRBG\"]=" + RBGExp + ","
+                            + "[\"aRBG\"]=" + acc_rbgexp + ","
+                            + "[\"lu\"]=" + "\"" + latest_update + "\""
+                            + "},"
+                            ;
+                        }
+
+
+                        if (acc_2v2exp != null && acc_3v3exp != null && acc_rbgexp != null)
+                        {
+                            outputLUA = "[\"" + userName + "-" + realm + "\"]={"
+                            + "[\"e2s\"]=" + twosExp + ","
+                            + "[\"a2s\"]=" + acc_2v2exp + ","
+                            + "[\"e3s\"]=" + tresExp + ","
+                            + "[\"a3s\"]=" + acc_3v3exp + ","
+                            + "[\"eRBG\"]=" + RBGExp + ","
+                            + "[\"aRBG\"]=" + acc_rbgexp + ","
+                            + "[\"lu\"]=" + "\"" + latest_update + "\""
+                            + "},"
+                            ;
+                        }
+
+
+                        string line = outputLUA;
+
+                        if (outputLUA != null && outputLUA != "")
+                        {
+                            sw.WriteLine(line);
+                        }
+                    }
+
+                    string endLua = "}";
+                    sw.WriteLine(endLua);
+                    sw.Close();
+                    AMPVP_ResetUI();
+                    MessageBox.Show("File converted succesfully. It can be found in the same folder as the JSON File.", "AMPVP Data Converter", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+            }
+            catch(Exception ex)
+            {
+                label1.Text = "An error occured, aborting...";
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        private void AMPVP_ConvertJSONTableFunc2(string json)
+        {
+
+            try
+            {
+
+                var jsonLinq = JObject.Parse(json);
+
+                var tempOutput = "";
+
+                var srcArray = jsonLinq.SelectToken("realms");
+
+                using (StreamWriter sw = new StreamWriter(origFileName + ".lua"))
+                {
+
+                    string startLua = "AMPVP_REALMLIST = { ";
+
+                    sw.WriteLine(startLua);
+
+                    foreach (var idk in srcArray)
+                    {
+                        //Console.WriteLine(idk);
+                        var realmName = idk.SelectToken("name");
+                        var realmSlug = idk.SelectToken("slug");
+                        Console.WriteLine(realmName + " - " + realmSlug);
+                        tempOutput = "[\"" + realmName + "\"]" + " = \"" + realmSlug + "\",";
+
+                        if (tempOutput != null && tempOutput != "")
+                        {
+                            sw.WriteLine(tempOutput);
+                        }
+
+                    }
+
+                   // Console.WriteLine(srcArray);
+                    
+
+                    string endLua = "}";
+                    sw.WriteLine(endLua);
+                    sw.Close();
+                    AMPVP_ResetUI();
+                    MessageBox.Show("File converted succesfully. It can be found in the same folder as the JSON File.", "AMPVP Data Converter", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "An error occured, aborting...";
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AMPVP_ConvertJSONTableFunc2(jsonString);
+        }
+    }
+}
